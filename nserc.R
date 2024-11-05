a87cded285410a87f5e5ca4f5e3b6f974a50f7e5
Arthur Leung <46766354+aleungplants@users.noreply.github.com>
2024-11-05 10:46:53 -0500

    Update nserc.R

--- nserc.R
+++ nserc.R
@@ -1,20 +1,23 @@
 library(magrittr)
 
 infiles <- list.files(path = here::here(),
-                     pattern = "*.xlsx")
+                     pattern = "[0-9].xls.xlsx")
 
 data <- purrr::map(infiles, ~ dplyr::mutate(readxl::read_xlsx(here::here(.), skip = 3),
                                             Filename = .)) %>%
   dplyr::bind_rows() %>%
   dplyr::mutate(Program = stringr::word(Filename, 1, sep = "_"),
                 CompetitionYear = stringr::word(`Fiscal Year`, 1, sep = "-"),
-                FirstName = stringr::word(Name, 2, sep = ", "),
-                LastName = stringr::word(Name, 1, sep = ", "),
                 .keep = "unused") %>%
   dplyr::select(Program, CompetitionYear, contains("Name"))
 data
 
-scholars <- data %>%
-  dplyr::rowwise() %>%
-  dplyr::mutate(ScholarID = scholar::get_scholar_id(LastName, FirstName))
+names <- data %>% head(3) %>% dplyr::pull(Name)
+scholar_ids <- list()
+for (name in names) {
+  scholar_ids <- list(scholar_ids, 
+                      name = scholar::get_scholar_id(last_name = stringr::word(name, 1, sep = ", "),
+                                                     first_name = stringr::word(name, 2, sep = ", ")))
+}
+
 writexl::write_xlsx(scholars, here::here("scholars.xlsx"))
